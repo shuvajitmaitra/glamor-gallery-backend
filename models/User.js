3704916d85a3eedc2d11323a28f495144296e05f
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-04-14 00:53:39 +0600

    Enhances User model with validation and security
    Adds email validation and password complexity requirements to the User model.
    
    Implements password hashing middleware using bcrypt to securely store user passwords.
    
    Introduces a method to compare provided passwords with the stored hash.

--- models/User.js
+++ models/User.js
@@ -1,21 +1,42 @@
-// models/User.js
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Invalid email format",
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: [8, "Password must be at least 8 characters"],
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  try {
+    if (this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
-// userSchema.pre("save", async function (next) {
-//   if (this.isModified("password")) {
-//     this.password = await bcrypt.hash(this.password, 10);
-//   }
-//   next();
-// });
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
-// userSchema.methods.comparePassword = function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
+userSchema.index({ email: 1 }, { unique: true });
 
 module.exports = mongoose.model("User", userSchema);
