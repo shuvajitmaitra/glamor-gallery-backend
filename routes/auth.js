6661ddec5ddaabb345a7ef0803c030112f72eae0
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-04-14 00:41:56 +0600

    Enables user model in authentication routes
    Uncomments the User model import in the authentication routes,
    allowing for user registration and login functionality.

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 // routes/auth.js
 const express = require("express");
 const router = express.Router();
-// const User = require("../models/User.js");
+const User = require("../models/User.js");
 // const jwt = require("jsonwebtoken");
 
 // router.post("/register", async (req, res) => {
