2b75c0e5078c8e922071a514f761de9a6c76f2be
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-04-14 23:45:41 +0600

    Enables user authentication with email and password
    Implements user authentication functionality by:
    
    - Validates user email and password upon registration.
    - Hashes user passwords before saving to the database for improved security.
    - Implements a method to compare provided passwords with stored hashed passwords during login.

--- models/User.js
+++ models/User.js
@@ -1,42 +1,43 @@
 const mongoose = require("mongoose");
-// const bcrypt = require("bcrypt");
-// const validator = require("validator");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
     email: {
       type: String,
-      required: true,
+      required: [true, "Email is required"],
       unique: true,
+      lowercase: true,
+      trim: true,
       validate: {
-        // validator: v,
+        validator: validator.isEmail,
         message: "Invalid email format",
       },
     },
     password: {
       type: String,
-      required: true,
+      required: [true, "Password is required"],
       minlength: [8, "Password must be at least 8 characters"],
     },
   },
   { timestamps: true }
 );
 
-// userSchema.pre("save", async function (next) {
-//   try {
-//     if (this.isModified("password")) {
-//       this.password = await bcrypt.hash(this.password, 10);
-//     }
-//     next();
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
-// userSchema.methods.comparePassword = async function (password) {
-//   return await bcrypt.compare(password, this.password);
-// };
-
-// userSchema.index({ email: 1 }, { unique: true });
+// Method to compare password
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
