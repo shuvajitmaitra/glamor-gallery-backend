63fe1d08178abb0cd223d00a69fa9453a7499179
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-04-14 23:49:19 +0600

    Simplifies user model
    Removes password hashing logic from the user model.
    
    The hashing and salting responsibilities are moved
    to a dedicated authentication service.
    This change keeps the user model lean and focused
    on data representation.

--- models/User.js
+++ models/User.js
@@ -1,43 +1,9 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const validator = require("validator");
 
-const userSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-      lowercase: true,
-      trim: true,
-      validate: {
-        validator: validator.isEmail,
-        message: "Invalid email format",
-      },
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-      minlength: [8, "Password must be at least 8 characters"],
-    },
-  },
-  { timestamps: true }
-);
-
-// Hash password before saving
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  try {
-    this.password = await bcrypt.hash(this.password, 10);
-    next();
-  } catch (error) {
-    next(error);
-  }
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
-
-// Method to compare password
-userSchema.methods.comparePassword = async function (password) {
-  return await bcrypt.compare(password, this.password);
-};
 
 module.exports = mongoose.model("User", userSchema);
