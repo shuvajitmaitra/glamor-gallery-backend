31904fa1fc97354ee2d53c67e74ff63a2cc1a1aa
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-04-20 23:40:54 +0600

    Removes name field from user schema
    Removes the name field from the user schema.
    
    The decision to remove this field stems from a simplification of user authentication and profile management, focusing on email and password for identification.

--- models/User.js
+++ models/User.js
@@ -1,15 +1,9 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = new mongoose.Schema(
   {
-    name: { type: String,
-      trim: true,
-      unique: true,
-      required: true
-},
-    email: { type: String, required: true, unique: true, trim: true, lowercase: true  },
-    password: { type: String, required: true, trim: true,  },
+    email: { type: String, required: true, unique: true, trim: true, lowercase: true },
+    password: { type: String, required: true, trim: true },
   },
   { timestamps: true }
 );
