3e3352bf676c904a10cf63d1fc244b7dd990bf3b
shuvajitmaitra <shuvajitmaitra@gmail.com>
2025-08-08 12:50:58 +0600

    role added

--- models/User.js
+++ models/User.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema(
     name: { type: String, trim: true, required: true },
     email: { type: String, required: true, unique: true, trim: true, lowercase: true },
     password: { type: String, required: true, trim: true },
+    role: { type: String, enum: ["user", "admin", "seller"], default: "user" },
   },
   { timestamps: true }
 );
